8e620aab99ccd78c3360a29d370da51c371cf031
Adeynack <Adeynack@users.noreply.github.com>
2020-12-03 01:09:23 +0100

    main manages errors
    + VS Code configuration

---
+++ rust/README.md
@@ -0,0 +1,10 @@
+m3ugen -- Rust Edition
+===
+
+## Develop This
+
+### Re-Run on Save
+
+```bash
+cargo watch -c -x "run ./doc/config_example.yaml"
+```
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,16 +2,31 @@ mod configuration;
 
 use configuration::Configuration;
 use simple_error::SimpleError;
-use std::fs;
+use std::{env, fs, process};
 
 fn main() {
-    println!("Hello, world!");
-    // let config_file_content = fs::read_to_string("./doc/config_example.yaml").expect("Error reading configuration file.");
-    // println!("config_file_content: {}", config_file_content);
-    // let config: Configuration = serde_yaml::from_str(&config_file_content).expect("Error deserializing configuration.");
-    let config =
-        load_configuration("./doc/config_example.yaml").expect("Error loading configuration");
+    if let Err(err) = main_or_error() {
+        eprintln!("{}", err.to_string());
+        process::exit(1);
+    }
+}
+
+fn main_or_error() -> Result<(), SimpleError> {
+    println!("---=== m3u Playlist Generator ===---");
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err(SimpleError::new(
+            "Expecting the configuration file as argument.",
+        ));
+    }
+    let config_filename = &args[1];
+    println!("Using configuration file at {}", config_filename);
+
+    let config = load_configuration(config_filename)?;
     println!("Configuration: {:?}", config);
+
+    Ok(())
 }
 
 fn load_configuration(configuration_file_path: &str) -> Result<Configuration, SimpleError> {
