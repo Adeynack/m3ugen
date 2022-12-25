34fc480dd90f2bf0c27cac028600079672b10c98
Adeynack <Adeynack@users.noreply.github.com>
2022-12-25 14:07:11 -0500

    refactor

--- rust/README.md
+++ rust/README.md
@@ -12,6 +12,11 @@ cargo install cargo-watch
 
 ### Re-Run on Save
 
+This will watch the code for changes and on every save:
+
+- lints code with _Clippy_
+- runs the program using the configuration file in the `doc` folder, named `config_example.yaml`.
+
 ```bash
-cargo watch -c -x "run ./doc/config_example.yaml"
+cargo watch -c -x "clippy -- -D warnings" -x "run ./doc/config_example.yaml"
 ```
--- rust/src/scan.rs
+++ rust/src/scan.rs
@@ -1,7 +1,7 @@
 use crate::configuration::Configuration;
 use eyre::eyre;
 use eyre::Result;
-use std::{collections::HashSet, error::Error, fs, path::Path};
+use std::{collections::HashSet, fs, path::Path};
 
 #[derive(Debug)]
 pub struct ScanResult {
@@ -21,49 +21,43 @@ impl ScanResult {
 pub fn scan(configuration: &Configuration) -> Result<ScanResult> {
     let mut scan_session = Scan {
         result: ScanResult::new(),
+        configuration,
     };
-    scan_session.start(configuration)?;
+    scan_session.start()?;
     Ok(scan_session.result)
 }
 
-struct Scan {
+struct Scan<'a> {
+    configuration: &'a Configuration,
     result: ScanResult,
 }
 
-impl Scan {
-    fn start(&mut self, configuration: &Configuration) -> Result<()> {
-        for folder in &configuration.scan_folders {
-            self.scan_folder(&configuration, Path::new(folder))
+impl Scan<'_> {
+    fn start(&mut self) -> Result<()> {
+        for folder in &self.configuration.scan_folders {
+            self.scan_folder(Path::new(folder))
                 .map_err(|e| eyre!("Unable to scan folder: {}", e))?;
         }
 
         Ok(())
     }
 
-    fn scan_folder(
-        &mut self,
-        configuration: &Configuration,
-        folder_path: &Path,
-    ) -> Result<(), Box<dyn Error>> {
+    fn scan_folder(&mut self, folder_path: &Path) -> Result<()> {
         let read_dir = fs::read_dir(folder_path)
             .map_err(|e| eyre!("Unable to read directory {:?}: {}", folder_path, e))?;
         for entry in read_dir {
             let path = entry?.path();
             if path.is_dir() {
-                self.scan_folder(&configuration, &path)?;
+                self.scan_folder(&path)?;
             } else {
-                self.consider_file_path(&configuration, &path)?;
+                self.consider_file_path(&path)?;
             }
         }
 
         Ok(())
     }
 
-    fn consider_file_path(
-        &mut self,
-        _configuration: &Configuration,
-        file_path: &Path,
-    ) -> Result<()> {
+    fn consider_file_path(&mut self, file_path: &Path) -> Result<()> {
         let file_path_str = file_path.to_str().unwrap();
         self.result.found_file_paths.push(file_path_str.to_string());
         Ok(())
